0a1694b6126eb55709952aa60f4e201962fa85c8
tamaramcgiffin <tamaramcgiffin@gmail.com>
2025-11-10 14:07:36 -0800

    Fixed config discreet URL

--- version-4/server/src/config.js
+++ version-4/server/src/config.js
@@ -1,6 +1,6 @@
 const config = {
   databaseUrl:
-    "postgresql://neondb_owner:npg_HPfsVwG13ceg@ep-weathered-frog-afsx5ey1-pooler.c-2.us-west-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+    "postgresql://neondb_owner:npg_b7aoZDIunPJ8@ep-weathered-frog-afsx5ey1-pooler.c-2.us-west-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
 };
 
 export default config;
