85dc799a53a845e9b9c863fcd5f26915de4116d5
tamaramcgiffin <tamaramcgiffin@gmail.com>
2025-11-10 10:43:56 -0800

    Set up server folder and config

--- version-4/server/src/config.js
+++ version-4/server/src/config.js
@@ -1,5 +1,8 @@
 const config = {
-  databaseUrl: "YOUR DATABASE CONNECTION STRING GOES HERE",
+  databaseUrl:
+    "postgresql://neondb_owner:npg_HPfsVwG13ceg@ep-weathered-frog-afsx5ey1-pooler.c-2.us-west-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
 };
 
 export default config;
+
+// psql 'postgresql://neondb_owner:npg_HPfsVwG13ceg@ep-weathered-frog-afsx5ey1-pooler.c-2.us-west-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require'
