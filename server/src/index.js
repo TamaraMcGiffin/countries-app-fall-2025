c91830542264b869133e966706bd529ed20b6bd0
tamaramcgiffin <tamaramcgiffin@gmail.com>
2025-11-18 13:37:53 -0800

    Added boilerplate code to index.js in server src folder

--- server/src/index.js
+++ server/src/index.js
@@ -1,3 +1,29 @@
+// ---------------------------------
+// Boilerplate Code to Set Up Server
+// ---------------------------------
+
+// importing Node Modules
+import express from "express";
+import pg from "pg"; // pg stands for PostgreSQL, for connecting to the database
+import config from "./config.js"; // importing the connection string to our database hosted on Neon
+
+//connecting to our PostgreSQL database, or db for short
+const db = new pg.Pool({
+  // new pg.Pool() creates a connection to the database
+  connectionString: config.databaseUrl, // credentials to access the database. Keep private!
+  ssl: true, // use SSL encryption when connecting to the database to keep data safe
+});
+
+const app = express(); // create an instance of the Express module, which gives us access to all of Express's functions, methods, useful superpowers
+
+app.use(express.json()); // This server will receive and respond to requests with JSON data
+
+const port = 3000; // Setting which port to listen or receive requests
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}!`);
+});
+
 /* --------------------------------
 Server/API for Countries App Version 4
 
