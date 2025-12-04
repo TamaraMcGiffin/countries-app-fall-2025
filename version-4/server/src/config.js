506ff03d64e9ba6c660f8493605fa7f60f76dd65
tamaramcgiffin <tamaramcgiffin@gmail.com>
2025-12-04 10:08:22 -0800

    Testing push

--- version-4/server/src/config.js
+++ version-4/server/src/config.js
@@ -1,8 +1,8 @@
 const config = {
   databaseUrl:
-    "postgresql://neondb_owner:npg_b7aoZDIunPJ8@ep-weathered-frog-afsx5ey1-pooler.c-2.us-west-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+    "postgresql://neondb_owner:npg_Jq2oPuSenj0i@ep-weathered-frog-afsx5ey1-pooler.c-2.us-west-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
 };
 
 export default config;
 
-// psql 'postgresql://neondb_owner:npg_HPfsVwG13ceg@ep-weathered-frog-afsx5ey1-pooler.c-2.us-west-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require'
+// Repushing new password, deleted git ignore from client and moved gitignore from server to Version 4 root folder
---
+++ version-5/client/src/pages/Home.jsx
@@ -0,0 +1,24 @@
+import CountryCard from "../components/CountryCard";
+
+// This line is the function name of Home for the Home.jsx page and it is passing through the countriesData prop
+// The countriesData prop stores the array of countries and it's details
+function Home({ countriesData }) {
+  // Here I am console logging countriesData to check that the information is coming through
+  // console.log(countriesData);
+
+  // Return means to render or display the items defined within the parentheses
+  return (
+    // Here is a div class named "countries-container" which will hold the cards
+    <div className="countries-container">
+      {/* Here I am using the map method to iterate over the array of countriesData and using the prop name "country" */}
+      {countriesData.map((country) => (
+        // Here I am assigning the data to the CountryCard component and using dot notation to identify the key
+        // and it's items for rendering back in the card
+        // country={country} is being useed here to assign the country prop to the CountryCard component
+        <CountryCard key={country.name.common} country={country} />
+      ))}
+    </div>
+  );
+}
+
+export default Home;
